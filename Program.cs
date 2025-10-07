04ef95f8fbe274e645d57c8ec2b099cbaee104f3
Augusto7y7 <augustos.rribeiro@gmail.com>
2025-10-07 14:27:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+int resuntado = 0;
+
+Console.WriteLine("Digite um número e vamos dizer o fatorial dele");
+n = int.Parse(Console.ReadLine());
+
+
+for (int i = i; i < n; i++) ;
+{
+    resultado = (n - 1) * n;
+}
+Console.WriteLine(resuntado);
